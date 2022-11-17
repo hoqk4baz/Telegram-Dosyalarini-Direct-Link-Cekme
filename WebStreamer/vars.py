b31250e24dc56485f1042f4e78b8b33c5c4f48bd
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2022-11-17 05:30:05 -0800

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -1,22 +1,22 @@
+
+
 from os import getenv, environ
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-
-
 class Var(object):
-    API_ID = int(getenv('API_ID', 19151088))
-    API_HASH = str(getenv('API_HASH', '0e07250efd85a5bab74b23472a8ed293'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '5666852175:AAF5yEFKkTu8BQ3UOcTmp1DcM1J3EhK_gL0'))
+    API_ID = int(getenv('API_ID'))
+    API_HASH = str(getenv('API_HASH'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN'))
     SESSION_NAME = str(getenv('SESSION_NAME', 'darkenza'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001893846131'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
     PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'trappleipalink-darkenza.koyeb.app'))
-    OWNER_ID = int(getenv('OWNER_ID', 5495065391))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+    OWNER_ID = int(getenv('OWNER_ID', '797848243'))
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     if 'DYNO' in environ:
@@ -24,10 +24,10 @@ class Var(object):
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://hoqk4baz:zsezsert55@cluster0.rvpzndm.mongodb.net/hoqk4baz?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL'))
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1000000000100")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001296894100")).split()))
